f78c28c03bd63e44029be0142d3095e1cfeef9e9
hairus <zhomenep@gmail.com>
2025-05-08 23:29:34 +0700

    gas

--- app/Http/Controllers/NurseController.php
+++ app/Http/Controllers/NurseController.php
@@ -3,32 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Events\PackageSent;
+use App\Models\tx_rx;
 use Illuminate\Http\Request;
 
 class NurseController extends Controller
 {
     public function infus($ruang, $bed)
     {
-        event(new PackageSent('infus', $ruang, $bed)); // Tanpa Queue, langsung jalankan
-        // PackageSent::dispatch('infus', $ruang, $bed);
+        // event(new PackageSent('infus', $ruang, $bed)); // Tanpa Queue, langsung jalankan
+        tx_rx::create([
+            "kamar" => 1,
+            "bed" => 1,
+            "pesan" => "infus"
+        ]);
+
+        PackageSent::dispatch('infus', $ruang, $bed);
+
     }
 
     public function perawat($ruang, $bed)
     {
-        event(new PackageSent('perawat', $ruang, $bed)); // Tanpa Queue, langsung jalankan
-        // PackageSent::dispatch('perawat', $ruang, $bed);
+        // event(new PackageSent('infus', $ruang, $bed)); // Tanpa Queue, langsung jalankan
+        PackageSent::dispatch('perawat', $ruang, $bed);
     }
 
     public function emergency($ruang)
     {
-        event(new PackageSent('emergency', $ruang, 0)); // Tanpa Queue, langsung jalankan
-        // PackageSent::dispatch('emergency', $ruang, 0);
+        PackageSent::dispatch('emergency', $ruang, 0);
     }
 
     public function cancel($ruang)
     {
-        event(new PackageSent('cancel', $ruang, 0)); // Tanpa Queue, langsung jalankan
-        // PackageSent::dispatch('cancel', $ruang, 0);
+        PackageSent::dispatch('cancel', $ruang, 0);
     }
 
 
---
+++ app/Http/Controllers/TxRxController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tx_rx;
+use Illuminate\Http\Request;
+
+class TxRxController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(tx_rx $tx_rx)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(tx_rx $tx_rx)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, tx_rx $tx_rx)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(tx_rx $tx_rx)
+    {
+        //
+    }
+}
